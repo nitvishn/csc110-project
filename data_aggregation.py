8c93ee4337bb1b0392f20bae1783f5a97c30e5d1
Aaron <634679m@gmail.com>
2021-12-13 17:17:45 -0500

    cleanup

--- data_aggregation.py
+++ data_aggregation.py
@@ -39,31 +39,6 @@ def load_posts(filename: str) -> list[RedditObject]:
             posts.append(RedditObject(clean_title(post['title']), post['score'], post['num_comments'], datetime.datetime.fromtimestamp(post['created_utc'])))
     return posts
 
-def scrape_reddit_data():
-    start = datetime.datetime(2010, 12, 1)
-    end = datetime.datetime.now()
-    resolution = datetime.timedelta(days=5)
-    posts = []
-    ratelimit = 120
-    while start + resolution <= end:
-        query = {
-            'fields': ['title','num_comments','score','created_utc','body'],
-            'before': int((start + resolution).timestamp()),
-            'after': int(start.timestamp()),
-            'size': 1000,
-            'subreddit': 'conspiracies'
-        }
-        response = requests.get(f"https://api.pushshift.io/reddit/search/submission/", params=query)
-        print(response.url)
-        data = json.loads(response.text)['data']
-        print(f"Completed {start}.")
-        posts.append(data)
-        start += resolution
-        with open('pushshift-reddit.json', 'w') as f:
-            json.dump(posts, f)
-        time.sleep(1)
-    return posts
-
 def load_covid_data(filename):
     with open(filename, 'r') as file:
         data = json.load(file)['OWID_WRL']
